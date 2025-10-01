bf73587aa22b6659a85fefa2e2cf7e8294b1e61c
Jonathan <jtalcantara.dev@gmail.com>
2025-10-01 16:34:07 -0300

    remocao de codigo temp

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,4 @@
 const express = require('express');
-const path = require('path');
-
-// Importar configurações centralizadas
-const config = require('../config');
 
 // Importar rotas específicas
 const leadsRoutes = require('./leads');
@@ -13,14 +9,5 @@ const router = express.Router();
 // Configurar rotas da API
 router.use('/auth', authRoutes);
 router.use('/leads', leadsRoutes);
-
-// Servir arquivos estáticos do frontend em produção
-if (config.server.nodeEnv === 'production') {
-  router.use(express.static(path.join(__dirname, '../../client/dist')));
-  
-  router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
-  });
-}
 
 module.exports = router;
